d270a3d15d19393a98a0707011f9dd0ed77945dc
nsahloum <sahloumi.najima@gmail.com>
2021-07-08 00:52:14 +0200

    do it for real

--- app/models/secret_menu_item.rb
+++
@@ -1,2 +0,0 @@
-class SecretMenuItem < ApplicationRecord
-end
--- db/migrate/20210707213858_create_secret_menu_items.rb
+++
@@ -1,9 +0,0 @@
-class CreateSecretMenuItems < ActiveRecord::Migration[6.1]
-  def change
-    create_table :secret_menu_items do |t|
-		t.string :menu_name
-		t.string :restaurant_name
-		t.string :menu_description
-    end
-  end
-end
