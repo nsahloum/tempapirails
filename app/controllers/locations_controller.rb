7df9d19818b84c92154a4c459875bc5aeb7edb56
nsahloum <sahloumi.najima@gmail.com>
2021-07-08 02:41:42 +0200

    CRUD location ok

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,37 @@
 class LocationsController < ApplicationController
+	def index
+        @locations = Location.all 
+        render json: @locations
+    end 
+
+    def show
+        @location = Location.find(params[:id])
+        render json: @location
+    end 
+
+    def create
+        @location = Location.create(
+            latitude: params[:latitude],
+            longitude: params[:longitude],
+            slug: params[:slug]
+        )
+        render json: @location
+    end 
+
+    def update
+        @location = Location.find(params[:id])
+        @location.update(
+            latitude: params[:latitude],
+            longitude: params[:longitude],
+            slug: params[:slug]
+        )
+        render json: @location
+    end 
+
+    def destroy
+        @locations = Location.all 
+        @location = Location.find(params[:id])
+        @location.destroy
+        render json: @locations
+    end 
 end
