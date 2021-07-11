449ecd0548d53748ba4954448c597a7070312d79
nsahloum <sahloumi.najima@gmail.com>
2021-07-12 00:25:11 +0200

    beginning of getting from the API

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,11 +1,12 @@
 class LocationsController < ApplicationController
 	
 	require 'rest-client'
+	require 'json'
 
 	def get_temp
 		url = "https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tz shift=0"
 		response = RestClient.get(url)
-		render json: response
+		response_hash = JSON.parse(response)
 	end
 	
 	def index
