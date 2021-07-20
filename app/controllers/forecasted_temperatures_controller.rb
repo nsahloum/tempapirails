796b8feb6bc000ea225698ba15f00294cb07bb9f
nsahloum <sahloumi.najima@gmail.com>
2021-07-20 23:08:02 +0200

    errors traite

--- app/controllers/forecasted_temperatures_controller.rb
+++ app/controllers/forecasted_temperatures_controller.rb
@@ -79,19 +79,34 @@ class ForecastedTemperaturesController < ApplicationController
 
 	
 	def show_location_temp
-		@location = Location.friendly.find(params[:location_id])
-		@long = @location.longitude
-		@lat = @location.latitude
-        @forecasted_temperatures = @location.forecasted_temperatures
-		if params[:start_date].present? && params[:end_date].present?
-			@start_date = params[:start_date].to_date
-			@end_date = params[:end_date].to_date
-			@forecasted_temperatures = @location.forecasted_temperatures.where(:date_forecasted => @start_date.beginning_of_day..@end_date.end_of_day)
+		if Location.where(slug: params[:location_id]).exists?
+			@location = Location.friendly.find(params[:location_id])
+			@long = @location.longitude
+			@lat = @location.latitude
+        	@forecasted_temperatures = @location.forecasted_temperatures
+			first_date = @forecasted_temperatures.first.date_forecasted
+			if params[:start_date].present? && params[:end_date].present?
+				@start_date = params[:start_date].to_date
+				@end_date = params[:end_date].to_date
+				@forecasted_temperatures = @location.forecasted_temperatures.where(:date_forecasted => @start_date..@end_date)
+			elsif params[:start_date].present?
+				@start_date = params[:start_date].to_date
+				@forecasted_temperatures = @location.forecasted_temperatures.where('date_forecasted >= ?', @start_date)
+			elsif params[:end_date].present?
+				@end_date = params[:end_date].to_date
+				@forecasted_temperatures = @location.forecasted_temperatures.where('date_forecasted <= ?', @end_date)
+			else
+				@forecasted_temperatures = @location.forecasted_temperatures
+			end
+		end
+		if @location == nil
+			render json: "Sorry no forecasted temperatures for this location"
+		elsif @forecasted_temperatures.first == nil
+			render json: "Sorry no forecasted temperatures for this date"
 		else
-			@forecasted_temperatures = @location.forecasted_temperatures
+			render json: @forecasted_temperatures.as_json(
+				only: [:date_forecasted, :min_forecasted, :max_forecasted]
+			)
 		end
-		render json: @forecasted_temperatures.as_json(
-			only: [:date_forecasted, :min_forecasted, :max_forecasted]
-		)
-		end
+	end
 end
