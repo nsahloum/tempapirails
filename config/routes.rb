a9c81c52d84d71e6628b8f13ea2d7ef13ddaa05d
nsahloum <sahloumi.najima@gmail.com>
2021-07-21 05:36:42 +0200

    some more comment

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   end
 
   #URL to synchronize all the locations or one location by adding location parameter : /synchronize?location=slug_name
+  #the CRON job can be send on this url
+  #example of crontab lanch every days at 18:30 :
+  # 30 18 * * * /usr/bin/curl -X POST http://127.0.0.1:3000/temperatures/synchronize
   post '/synchronize' => 'application#get_data'
 
   #getting all forecasted temperatures for one location
