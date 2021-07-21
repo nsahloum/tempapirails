cf1d7d2d43e1fc8338b88b1b663779fcc94c7933
nsahloum <sahloumi.najima@gmail.com>
2021-07-21 15:56:54 +0200

    beginning date validation

--- app/controllers/forecasted_temperatures_controller.rb
+++ app/controllers/forecasted_temperatures_controller.rb
@@ -1,21 +1,38 @@
 class ForecastedTemperaturesController < ApplicationController
 
 	#this method shows forecasted temperature for one location. url GET /slug_name
+	require 'date'
+	# def validate_date(datum)
+	# 	if Date.valid_date?(datum) == true
+	# 		return true
+	# 	else
+	# 		render json: "ERROR:5 : Date format no valid"
+	# 	end
+	# end
+
+	def validate_date?(date)
+  		date_format = '%Y-%m-%d'
+  		DateTime.strptime(date, date_format)
+ 	 	true
+	rescue ArgumentError
+  		false
+	end
+
 	def show_location_temp
 		if Location.where(slug: params[:location_id]).exists?
 			@location = Location.friendly.find(params[:location_id])
         	@forecasted_temperatures = @location.forecasted_temperatures
-			if params[:start_date].present? && params[:end_date].present? #to filter by start date and end date, url GET /slug_name?start_date=""&end_date=""
+			if params[:start_date].present? && params[:end_date].present? && validate_date?(params[:start_date]) && validate_date?(params[:end_date])#to filter by start date and end date, url GET /slug_name?start_date=""&end_date=""
 				@start_date = params[:start_date].to_date
 				@end_date = params[:end_date].to_date
 				@forecasted_temperatures = @location.forecasted_temperatures.where(:date_forecasted => @start_date..@end_date)
-			elsif params[:start_date].present? #show all the forecasted temperature from start date (if only start_date specified)
+			elsif params[:start_date].present? && validate_date?(params[:start_date]) #show all the forecasted temperature from start date (if only start_date specified)
 				@start_date = params[:start_date].to_date
 				@forecasted_temperatures = @location.forecasted_temperatures.where('date_forecasted >= ?', @start_date)
-			elsif params[:end_date].present? #show all the forecasted temperature before end date (if only end_date specified)
+			elsif params[:end_date].present? && validate_date?(params[:end_date]) #show all the forecasted temperature before end date (if only end_date specified)
 				@end_date = params[:end_date].to_date
 				@forecasted_temperatures = @location.forecasted_temperatures.where('date_forecasted <= ?', @end_date)
-			else
+			elsif params[:start_date].present? == false && params[:end_date].present? == false
 				@forecasted_temperatures = @location.forecasted_temperatures #if no params, show all the forecasted_temepratures for a location
 			end
 		end
